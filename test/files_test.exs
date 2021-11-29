75cdcc59b4a8d52dc7e68262cad8cf71915ff47d
John Davenport <johns10davenport@gmail.com>
2021-11-28 19:55:12 -0500

    Update test

--- test/files_test.exs
+++ test/files_test.exs
@@ -17,6 +17,7 @@ defmodule Docpipe.FilesTest do
   defp create_local_files() do
     File.touch(@path <> "/file1.html")
     File.touch(@path <> "/file2.html")
+    File.touch(@path <> "/file3.pdf")
     File.mkdir(@path <> "/test_dir")
     File.touch(@path <> "/test_dir/file3.html")
     File.touch(@path <> "/test_dir/file4.html")
@@ -25,6 +26,7 @@ defmodule Docpipe.FilesTest do
   defp delete_local_files() do
     File.rm(@path <> "/file1.html")
     File.rm(@path <> "/file2.html")
+    File.touch(@path <> "/file3.pdf")
     File.mkdir(@path <> "/test_dir/file3.html")
     File.mkdir(@path <> "/test_dir/file4.html")
     File.rmdir(@path <> "/test_dir")
@@ -34,7 +36,6 @@ defmodule Docpipe.FilesTest do
     alias Docpipe.Files.File
 
     setup [
-      :start_db,
       :create_dirs
     ]
 
@@ -74,6 +75,12 @@ defmodule Docpipe.FilesTest do
     test "list/1 returns all files" do
       create_local_files()
       files = Files.list([source: :file_system])
+      assert files == ["file3.pdf", "file2.html", "file1.html", "test_dir/file4.html", "test_dir/file3.html"]
+    end
+
+    test "list/1 with filter opts returns files with the specified extension" do
+      create_local_files()
+      files = Files.list([source: :file_system, filters: [extensions: ["html"]]])
       assert files == ["file2.html", "file1.html", "test_dir/file4.html", "test_dir/file3.html"]
     end
 
@@ -84,50 +91,5 @@ defmodule Docpipe.FilesTest do
       }
       assert Files.cast_file_system("/test_dir/file_4.html") == result
     end
-
-    """
-
-    test "get_project!/1 returns the project with given id" do
-      project = project_fixture()
-      assert Projects.get_project!(project.id) == project
-    end
-
-    test "create_project/1 with valid data creates a project" do
-      strategy = UserDocs.WebFixtures.strategy()
-      team = UserDocs.UsersFixtures.team()
-      attrs = @valid_attrs |> Map.put(:strategy_id, strategy.id) |> Map.put(:team_id, team.id)
-      assert {:ok, %Project{} = project} = Projects.create_project(attrs)
-      assert project.base_url == "some base_url"
-      assert project.name == "some name"
-    end
-
-    test "create_project/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Projects.create_project(@invalid_attrs)
-    end
-
-    test "update_project/2 with valid data updates the project" do
-      project = project_fixture()
-      assert {:ok, %Project{} = project} = Projects.update_project(project, @update_attrs)
-      assert project.base_url == "some updated base_url"
-      assert project.name == "some updated name"
-    end
-
-    test "update_project/2 with invalid data returns error changeset" do
-      project = project_fixture()
-      assert {:error, %Ecto.Changeset{}} = Projects.update_project(project, @invalid_attrs)
-      assert project == Projects.get_project!(project.id)
-    end
-
-    test "delete_project/1 deletes the project" do
-      project = project_fixture()
-      assert {:ok, %Project{}} = Projects.delete_project(project)
-      assert_raise Ecto.NoResultsError, fn -> Projects.get_project!(project.id) end
-    end
-
-    test "change_project/1 returns a project changeset" do
-      project = project_fixture()
-      assert %Ecto.Changeset{} = Projects.change_project(project)
-    end
-    """
   end
 end
